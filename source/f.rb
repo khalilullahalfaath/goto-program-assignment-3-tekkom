d91824d55bccdf28f998c96c6296edbc2683ea09
@khalilullah.alfaath21 <khalilullah.alfaath21@gmail.com>
2026-09-24 08:28:16 +0700

    Ringkas laporan, tambah gambar jawaban, dan rapikan WHILE program
    - main.tex: hapus bagian Review Program GOTO dan listing GOTO yang
      ditulis manual; program GOTO 3.1 dan 3.3 kini disajikan sebagai
      gambar. Penjelasan tiap problem dipadatkan (11 -> 4 halaman).
    - main.tex: Problem 3.2 memakai satu screenshot Ruby playground yang
      memuat kode sekaligus hasil eksekusinya.
    - source/f.rb: tambah mode baterai uji (tanpa argumen) di samping mode
      pemanggilan f(x1, x2), dan sederhanakan komentar per label.
    - images/: tambah gambar program GOTO 3.1, program GOTO 3.3, dan
      screenshot Ruby playground.

--- source/f.rb
+++ source/f.rb
@@ -1,84 +1,50 @@
-# f.rb
-# Implementasi Ruby dari WHILE program hasil terjemahan mekanis
-# (skema Teorema 3.3, Lecture 3 slide 55-56) dari GOTO program f
-# pada Problem 3.2. Variabel x3 berperan sebagai variabel kontrol
-# "y" (program counter) pada skema terjemahan.
+# f.rb - WHILE program untuk Problem 3.2 (hasil terjemahan GOTO -> WHILE)
+# x1, x2 = input, x0 = output, x3 = program counter
+# Tanpa argumen: baterai uji; dengan 2 argumen: hitung f(x1, x2).
 
-x1 = ARGV[0].to_i
-x2 = ARGV[1].to_i
-
-x0 = 0
-x3 = 0
-
-# x3 := 1
-while x3 != 0
-  x3 = x3 - 1
+kasus = [[0, 0], [0, 5], [5, 0], [1, 3], [2, 1], [4, 4], [5, 2], [7, 3], [9, 4], [4, 9]]
+if ARGV.size >= 2
+  kasus = [[ARGV[0].to_i, ARGV[1].to_i]]
 end
-x3 = x3 + 1
 
-while x3 != 0
+kasus.each do |x1, x2|
+  a1, a2 = x1, x2           # input asli, untuk dicetak
+  x0 = 0
+  x3 = 1                    # mulai dari label L1
 
-  # IF x3 = 1 THEN
-  #   IF x1 = 0 THEN x3 := 9 ELSE x3 := 2 END
-  # END
-  if x3 == 1
-    if x1 == 0
-      x3 = 9
-    else
+  while x3 != 0
+    if x3 == 1              # L1: cek x1 = 0
+      x3 = x1 == 0 ? 9 : 2
+    end
+    if x3 == 2              # L2: x0 := x0 + 1
+      x0 = x0 + 1
+      x3 = 3
+    end
+    if x3 == 3              # L3: cek x2 = 0
+      x3 = x2 == 0 ? 9 : 4
+    end
+    if x3 == 4              # L4: x0 := x0 + 1
+      x0 = x0 + 1
+      x3 = 5
+    end
+    if x3 == 5              # L5: x2 := x2 - 1
+      x2 = x2 - 1
+      x3 = 6
+    end
+    if x3 == 6              # L6: x1 := x1 - 1
+      x1 = x1 - 1
+      x3 = 7
+    end
+    if x3 == 7              # L7: GOTO L1
+      x3 = 1
+    end
+    if x3 == 8              # L8: GOTO L2 (dead code)
       x3 = 2
     end
-  end
-
-  # IF x3 = 2 THEN x0 := x0 + 1; x3 := 3 END
-  if x3 == 2
-    x0 = x0 + 1
-    x3 = 3
-  end
-
-  # IF x3 = 3 THEN
-  #   IF x2 = 0 THEN x3 := 9 ELSE x3 := 4 END
-  # END
-  if x3 == 3
-    if x2 == 0
-      x3 = 9
-    else
-      x3 = 4
+    if x3 == 9              # L9: HALT
+      x3 = 0
     end
   end
 
-  # IF x3 = 4 THEN x0 := x0 + 1; x3 := 5 END
-  if x3 == 4
-    x0 = x0 + 1
-    x3 = 5
-  end
-
-  # IF x3 = 5 THEN x2 := x2 - 1; x3 := 6 END
-  if x3 == 5
-    x2 = x2 - 1
-    x3 = 6
-  end
-
-  # IF x3 = 6 THEN x1 := x1 - 1; x3 := 7 END
-  if x3 == 6
-    x1 = x1 - 1
-    x3 = 7
-  end
-
-  # IF x3 = 7 THEN x3 := 1 END
-  if x3 == 7
-    x3 = 1
-  end
-
-  # IF x3 = 8 THEN x3 := 2 END  (baris mati, L8 tidak pernah dituju)
-  if x3 == 8
-    x3 = 2
-  end
-
-  # IF x3 = 9 THEN x3 := 0 END
-  if x3 == 9
-    x3 = 0
-  end
-
+  puts "f(#{a1}, #{a2}) = #{x0}"
 end
-
-puts "f(#{ARGV[0]}, #{ARGV[1]}) = #{x0}"
